47fa06e7762deaa1949fac5cfa451aed6c50ad1c
IlyaTelenkov <atomnato00000@gmail.com>
2022-03-31 17:52:15 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/tz_laborato/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tz_laborato
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Application());
+}
+
+class Application extends StatelessWidget {
+  const Application({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Container(),
+    );
+  }
+}
